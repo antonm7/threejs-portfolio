d5b1474944d86313828005c8f8e95b1c2277d00d
antonm7 <migolkoanton@gmail.com>
2023-04-25 19:34:14 +0200

    new component for logo

---
+++ src/components/PurpleWrapper/index.tsx
@@ -0,0 +1,11 @@
+import styles from './index.module.scss';
+import A_white from '/assets/general/a_white.svg'
+
+export default function PurpleWrapper() {
+    return (
+        <div id={styles.wrapper} className="flex flex-nowrap bg-mainPurple rounded-2xl w-[230px] h-[84px] relative">
+            <img src={A_white} className="h-8 pr-[3px] absolute" id={styles.a_white}/>
+            <span className="font-bold font-integral text-white absolute" id={styles.nton_white}>NTON</span>
+        </div>
+    )
+}

d3ebe6ee21be8a2cfb771c958b0195d6f2a7637d
antonm7 <migolkoanton@gmail.com>
2023-06-05 15:32:24 +0300

    returned the layout animation

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,19 +1,31 @@
 import ScrollToTop from '../../helpers/scrollToTop'
 import useWidth from '../../helpers/useWindowSize'
 import styles from './index.module.scss'
+import {motion} from 'framer-motion'
+
+const animation = {
+    initial: {opacity:0 },
+    animate:{opacity:1},
+    exit:{opacity:0}
+}
 
 const Layout = ({children}:any) => {
-    
     const width = useWidth()
     
     if (width && width >= 601) {
         return (
-            <div id={styles.width_container} className="absolute z-50 w-[52vw] p-4">
-               <ScrollToTop />
-               <div className={`z-[0] w-full h-full rounded-3xl p-16 overflow-auto`} id={styles.wrapper}>
+            <motion.div 
+            variants={animation} 
+            initial="initial" 
+            animate="animate"
+            exit="exit"
+            id={styles.width_container}
+            className="absolute z-50 w-[52vw] p-4" >
+                <div className={`z-[0] w-full h-full rounded-3xl p-16 overflow-auto`} id={styles.wrapper}>
+                    <ScrollToTop />
                     {children}
                 </div>
-           </div>
+           </motion.div>
         )
     } else {
          return (
